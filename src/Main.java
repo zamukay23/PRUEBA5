6f1382093c60c38240004bdbdcbcc67b02c0e46a
Alex Reyes <JonathanRocky7@gmail.com>
2025-05-13 16:44:52 -0500

    "alex reyes"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package <default package>; 
 /**
  *
  * @author User
@@ -15,5 +15,6 @@ public class Main {
         /*Josue Guevara*/
         /*Anthony Semblantes*/
         /*Jonathan Lopez*/
+        /*Alex Reyes*/
     }
 }
