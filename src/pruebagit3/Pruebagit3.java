000803e18b1e705f38902feece72d0ad69557339
Anthony <Anthony@DESKTOP-SRS1SFS>
2025-05-13 20:03:06 -0500

    esto es todo

---
+++ src/pruebagit3/Pruebagit3.java
@@ -0,0 +1,16 @@
+    
+package pruebagit3;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Pruebagit3 {
+
+    public static void main(String[] args) {
+            System.out.println("Esto es ");
+
+
+    }
+    
+}
