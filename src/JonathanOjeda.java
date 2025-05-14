04dc025207bb0efd704833affa981bb58ff02db8
Ajkarots <jonat@10.79.15.250>
2025-05-14 09:36:05 -0500

    Se crea clase JonathanOjeda

---
+++ src/JonathanOjeda.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jonat
+ */
+public class JonathanOjeda {
+    String nombre="Jonathan";
+    String apellido="Ojeda";
+    
+    public void imprimir(){
+            System.out.println(nombre);
+            System.out.println(apellido);
+}
+}
