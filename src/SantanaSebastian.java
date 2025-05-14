03fecebf99877d76aba6f7be506cb34f0024b6ad
SebasIsd <sdsnt2003@gmail.com>
2025-05-13 21:57:59 -0500

    SantanaSebastian commit

---
+++ src/SantanaSebastian.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author S3Bzs
+ */
+public class SantanaSebastian {
+    public static void main(String[] args) {
+        System.out.println("Sebastian");
+        System.out.println("Santana");
+    }
+}
