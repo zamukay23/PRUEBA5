df6f7e321d6d292f6d9065bd32c464b88b73cecf
User Josue Guevara <>
2025-05-13 23:30:46 -0500

    Clase Josue Guevara

---
+++ src/JosueGuevara.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class JosueGuevara {
+    private String nombre="Josue";
+    private String apellido="Guevara";
+    public void imprimirNombres(){
+        System.out.println(nombre);
+        System.out.println(apellido);
+    }
+}
