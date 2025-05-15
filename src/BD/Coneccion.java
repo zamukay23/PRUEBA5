0f58e082fb6eb0ab48eb5b724f4f834d9923e400
Joshua <el.jdragon@gmail.com>
2025-05-15 12:00:00 -0500

    Carpetas
    Se elimino lo anteiror y se pusieron las carpetas correctas con una clase genrica par que el rpeositorio permita subirlas

--- src/BD/Coneccion.java
+++ src/BD/Coneccion.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package BD;
 
 /**
  *
- * @author jonat
+ * @author User
  */
-public class JonathanOjeda {
-    String nombre="Jonathan";
-    String apellido="Ojeda";
+public class Coneccion {
     
-    public void imprimir(){
-            System.out.println(nombre);
-            System.out.println(apellido);
-}
 }
